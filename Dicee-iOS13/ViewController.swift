b0f86586b9e9a84e56bf705b18597f3a2c9c244d
Donald Ho <Donald.h.ho@gmail.com>
2022-08-21 20:10:28 +1000

    4 dices roll at pressing

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImageView1: UIImageView!
+    @IBOutlet var diceImageView2: UIImageView!
+    @IBOutlet var diceImageView3: UIImageView!
+    @IBOutlet var diceImageView4: UIImageView!
+    
+    var diceNumber1: Int?
+    var diceNumber2: Int?
+    var diceNumber3: Int?
+    var diceNumber4: Int?
+    
+    @IBAction func rollBtnPressed(_ sender: UIButton) {
+        diceNumber1 = Int.random(in: 1...6)
+        diceNumber2 = Int.random(in: 1...6)
+        diceNumber3 = Int.random(in: 1...6)
+        diceNumber4 = Int.random(in: 1...6)
+        diceImageView1.image = UIImage(named: numberToName(diceNumber1!))
+        diceImageView2.image = UIImage(named: numberToName(diceNumber2!))
+        diceImageView3.image = UIImage(named: numberToName(diceNumber3!))
+        diceImageView4.image = UIImage(named: numberToName(diceNumber4!))
+        
+    }
+    
+    func numberToName(_ diceNumber: Int)-> String{
+        switch diceNumber{
+        case 1:
+            return "DiceOne"
+        case 2:
+            return "DiceTwo"
+        case 3:
+            return "DiceThree"
+        case 4:
+            return "DiceFour"
+        case 5:
+            return "DiceFive"
+        default:
+            return "DiceSix"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
